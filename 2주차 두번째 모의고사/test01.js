ec8aab6a0dc0393fd4c105c09963fc92b7d98156
kmdet1235 <73745204+kmdet1235@users.noreply.github.com>
2022-09-30 09:49:24 +0900

    2주차 두번째 알고리즘 테스트

--- 2주차 두번째 모의고사/test01.js
+++ 2주차 두번째 모의고사/test01.js
@@ -1,7 +1,10 @@
+// 1번문제. 없는 정수 사이의 합
+// 0부터 9까지 숫자 중 일부가 들어있는 정수배열이 매개변수로 주어지고, 정수배열 내에 없는 숫자들의 합을 구하는 문제
+
 function solution(arr1) {
-  let answer = 45;
+  let answer = 45; // 0~9까지의 합이 45
   for (let i = 0; i < arr1.length; i++) {
-    answer -= arr1[i];
+    answer -= arr1[i]; // 45에서 배열속에 있는 숫자를 빼면 없는 숫자들의 합이 나옴
   }
   return answer;
 }
